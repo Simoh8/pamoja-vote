609489022be39de162a6101331898de4483e6ca8
simoh8 <simomutu8@gmail>
2025-10-23 13:30:23 +0300

    Add dj-database-url and psycopg2 for Neon DB fixed

--- backend/pamoja_vote/settings.py
+++ backend/pamoja_vote/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv(
     'ALLOWED_HOSTS',
-    'localhost,127.0.0.1'
+    '.vercel.app,localhost,127.0.0.1'
 ).split(',')
 
 # ----------------------------------------------------------------------
